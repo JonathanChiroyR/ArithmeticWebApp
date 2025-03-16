0e6962c263a0594c8a1ea6640c5db65b1b4718d2
JonatanChiroyR <j.chiroyrivera@gmail.com>
2025-03-16 17:47:45 -0600

    Backend Project Initializing

---
+++ arithmeticOpsBackend/src/main/java/com/jonathancr/arithmeticOpsBackend/ArithmeticOpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jonathancr.arithmeticOpsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticOpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmeticOpsBackendApplication.class, args);
+	}
+
+}
---
+++ arithmeticOpsBackend/src/test/java/com/jonathancr/arithmeticOpsBackend/ArithmeticOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathancr.arithmeticOpsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
